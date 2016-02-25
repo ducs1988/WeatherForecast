e823949d6c1a5b504eec2c690e397cdb05d24341
ducs1988 <100200dcs>
2016-02-25 00:01:31 -0600

    Add intent into DetailActivity

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -13,7 +13,7 @@ public class DetailActivity extends ActionBarActivity {
         setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new DetailActivityFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
